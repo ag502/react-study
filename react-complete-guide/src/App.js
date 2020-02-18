4c314754a8ca5423777b7018db886610db0fcba7
ag502 <ag502@naver.com>
2020-02-18 13:31:33 +0900

    Create Functional Component
    There are two ways to make component in react.
    One is using class, the other is using function.
    We usually use functional component making stateless component.

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really Work</p>
+        <Person />
+      </div>
+    );
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hi I'm React App")
+    // );
+  }
+}
+
+export default App;
