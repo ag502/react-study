932372d4fb64ca26ccad111622b04f68da968428
ag502 <ag502@naver.com>
2020-02-18 13:43:46 +0900

    Output Dynamic Content
    Use property to make component more dynamically.
    Also We should use curly bracket to use Javascript code in jsx.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,14 +9,13 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really Work</p>
-        <Person />
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">
+          My Hobbies: Racing
+        </Person>
+        <Person name="Stephanie" age="26" />
       </div>
     );
-    // return React.createElement(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement("h1", null, "Hi I'm React App")
-    // );
   }
 }
 
