d72894e5d94eab55697cf0d793e082c1f74f5aec
이재윤 <ag502@naver.com>
2021-09-09 12:43:47 +0900

    delete package file

--- react-tutorial/hook/src/App.js
+++ react-tutorial/hook/src/App.js
@@ -4,11 +4,23 @@ import logo from './logo.svg';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [size, setWindowSize] = useState({height: window.innerHeight, width: window.innerWidth})
 
   useEffect(() => {
       document.title = `You clicked ${count} times`
       return () => console.log('a')
-  }, [count])
+  }, [count]);
+
+  useEffect(() => {
+      const handleWindowSize = () => {
+          setWindowSize({height: window.innerHeight, width: window.innerWidth})
+      }
+
+      window.addEventListener('resize', handleWindowSize)
+      return () => {
+          window.removeEventListener('resize', handleWindowSize)
+      }
+  }, [size])
 
   return (
     <div className="App">
